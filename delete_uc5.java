0927d694c4fc500e0992323684112fba9789bc48
Murtazamalik1 <113129901+Murtazamalik1@users.noreply.github.com>
2022-11-16 19:59:37 +0530

    linked list

---
+++ delete_uc5.java
@@ -0,0 +1,28 @@
+package com.linkedlist;
+import java.util.*;
+
+public class delete_uc5<I extends Number> {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> Stack = new Stack<Integer>();
+
+      /*  Stack.add(56);
+        Stack.add(30);
+        Stack.add(70);
+
+
+        //Stack = 0->2->5->3->1
+        System.out.println("following elements before remove: " + Stack);
+        System.out.println("The element removed from the front of Stack: "+Stack.pop());
+        System.out.println("Following are the elements in Stack after removing: " + Stack);
+
+    }*/
+
+        Stack.push(56);
+        Stack.push(30);
+        Stack.push(70);
+        System.out.println("The element removed from the front of Stack: "+Stack.pop());
+        System.out.println("following elements before remove: " + Stack);
+    }
+    }
