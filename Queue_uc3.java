524f639b5c0768975d69f2bcb0ad993fddfe5fee
Murtazamalik1 <113129901+Murtazamalik1@users.noreply.github.com>
2022-11-16 21:40:13 +0530

    Queue

---
+++ Queue_uc3.java
@@ -0,0 +1,15 @@
+package stack_and_queue;
+import java.util.LinkedList;
+import java.util.*;
+import java.util.Queue;
+
+public class Queue_uc3 {
+    public static void main(String[] args) {
+
+         Queue<Integer> myqueue = new LinkedList<>();
+         myqueue.add(56);
+         myqueue.add(30);
+         myqueue.add(70);
+         System.out.println(""+myqueue);
+    }
+}
